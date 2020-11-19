9fc2b2967e46fe3b007e8b9d8b252174f45d6867
Austin Jackson <themacphage@gmail.com>
2020-11-19 02:00:00 -0600

    regular job running works

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,51 @@
 #!/usr/bin/env ts-node-script
 
-console.log('Hello, index.ts!');
+import express from 'express';
+import Queue from 'bull';
+import { setQueues, UI } from 'bull-board';
+import { snooze } from '@au5ton/snooze';
 
-import puppeteer from 'puppeteer'
+type Foo = { message: string }
 
-(async () => {
-    let browser = await puppeteer.launch({
-        args: [
-            '--headless',
-            '--no-sandbox',
-            '--disable-gpu',
-            '--disable-dev-shm-usage',
-            '--disable-setuid-sandbox'
-        ]
-    });
+const jobs = new Queue<Foo>('Jobs');
+setQueues([jobs]);
 
-    const browserVersion = await browser.version();
-    console.log(`Started ${browserVersion}`);
+jobs.process(async (job) => {
+  let progress = 0;
+  for(let i = 0; i < 100; i++) {
+    await snooze(100);
+    progress += 1;
+    await job.progress(progress);
+  }
+  console.log(`job message: ${job.data.message}`);
+  return;
+});
 
-    let page = await browser.newPage();
-    await page.close();
-    await browser.close();
-})();
+jobs.add({ message: 'Hello, World!' }, { repeat: { cron: '* * * * *' }})
+
+//import puppeteer from 'puppeteer';
+
+const app = express();
+app.use('/', UI);
+app.listen(1234, () => {
+  console.log(`Example app listening at http://localhost:1234`);
+});
+
+// (async () => {
+//   let browser = await puppeteer.launch({
+//     args: [
+//       '--headless',
+//       '--no-sandbox',
+//       '--disable-gpu',
+//       '--disable-dev-shm-usage',
+//       '--disable-setuid-sandbox'
+//     ]
+//   });
+
+//   const browserVersion = await browser.version();
+//   console.log(`Started ${browserVersion}`);
+
+//   let page = await browser.newPage();
+//   await page.close();
+//   await browser.close();
+// })();
