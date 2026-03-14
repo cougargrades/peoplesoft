9dff2536bf20d9aab560dd6e9127475b41855505
Austin Jackson <themacphage@gmail.com>
2026-03-14 17:52:10 -0500

    added empty test and combined workflows so release can't happen without passed tests

---
+++ test/empty.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest'
+
+// import { AtomLink, AtomPerson, AtomText } from '../src/index';
+// import * as Parser from '../src/Parser';
+// import { isAtomFeed, readFileSync } from './util';
+
+describe('empty', () => {
+  it('should always pass', () => {
+    expect(true).toEqual(true);
+  })
+});
